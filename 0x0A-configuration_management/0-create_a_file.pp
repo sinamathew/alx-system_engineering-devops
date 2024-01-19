e6868418bafe8f0543077c11bc742657d145e91e
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-01-20 00:33:41 +0100

    Fixed Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creates the file school im tmp
 file {'/tmp/school':
-ensure  => 'file'
+content => 'I love Puppet',
+ensure  => 'file',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
-content => 'I love Puppet'
 }
