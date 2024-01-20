f7232d4b78bd68f59da0400ee859acea0afbcddd
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-01-20 01:34:13 +0100

    Pupp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+# Ensure the necessary provider is available
+package { 'puppetlabs-python':
+  ensure => present,
+}
+
+# Install Flask 2.1.0 using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
+}
+
